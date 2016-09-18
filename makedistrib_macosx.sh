18865067f7e295c29c81805c1bcd9bbbfd0a99c8
Paul Batchelor <thisispaulbatchelor@gmail.com>
2016-09-17 19:07:59 -0700

    removed makedistrib

--- makedistrib_macosx.sh
+++
@@ -1,4 +0,0 @@
-cd release/distrib/
-zip -vr ptk_v2.5.4_macosx_svn_666.zip * -x@../../exclude_macosx.lst
-cd ..
-cd ..
--- makedistrib_window.bat
+++
@@ -1,5 +0,0 @@
-cd release\distrib\
-..\..\zip -vr ptk_v2.5.3_windows_svn_653.zip * -x@..\..\exclude_windows.lst
-cd ..
-cd ..
-
